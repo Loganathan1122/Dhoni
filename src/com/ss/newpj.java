1c982573a81ece6df0f32b90dd5caa0850e76443
Loganathan1122 <l.komathiloganathan@gmail.com>
2024-09-02 17:33:53 +0530

    dhoniproject
    this is dhoni projetc

---
+++ src/com/ss/newpj.java
@@ -0,0 +1,14 @@
+package com.ss;
+
+public class newpj {
+	
+private static void room1() {
+	// TODO Auto-generated method stub
+	System.out.println("hello java");
+
+}
+public static void main(String[] args) {
+	newpj.room1();
+}
+
+}
