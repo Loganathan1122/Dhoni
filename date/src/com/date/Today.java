da85e1afabc2129e56a4f7fdfab8c2379afc0721
Loganathan1122 <l.komathiloganathan@gmail.com>
2024-09-04 10:23:09 +0530

    hi
    hi

--- date/src/com/date/Today.java
+++ date/src/com/date/Today.java
@@ -1,9 +1,28 @@
 package com.date;
-
+import org.openqa.selenium.EdgeDriver;
+import org.openqa.selenium.WebDriver;
 public class Today {
 	
-	private void dob() {
-	System.out.println("date of birth");
+	public static void main(String[] args) {
+		System.setProperty("webdriver.edge.driver", "C:\\Users\\LOGANATHAN\\eclipse-workspace\\New folder\\Dhoni\\date\\msedgedriver.exe");
+		
+		 WebDriver driver = new EdgeDriver();
+		 driver
+		 
+		
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
